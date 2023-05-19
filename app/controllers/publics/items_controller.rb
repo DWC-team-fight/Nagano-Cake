96d0f7ad79d29c75f980449c86b559e2e5364219
chihiro0401 <129600067+chihiro0401@users.noreply.github.com>
2023-05-19 06:22:58 +0000

    更新

--- app/controllers/publics/items_controller.rb
+++ app/controllers/publics/items_controller.rb
@@ -1,9 +1,13 @@
 class Publics::ItemsController < ApplicationController
   
   
-  def index
-    
+  def show
+    @item = Item.find(parems[:id])
+    @care_item = CartItem.new
+    if customer_signed_in? 
+      @customer_item = CartItenm.where(customer_id:[current_customer.id])
+    end
   end
   
-  def show
-end
+  def index
+    
