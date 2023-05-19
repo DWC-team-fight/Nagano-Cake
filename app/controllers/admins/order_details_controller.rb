cb91937c202c732b9419c4668bd6ed5085fd320c
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-05-19 14:42:08 +0000

    update admin_customers_all

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,2 +1,22 @@
 class Admins::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def update
+    @order_detail=OrderDetail.find(params[:id])
+    @prder_detail.update(order_detail_params)
+    @order=Order.find(params[:order_id])
+    if @order.order_details.any? {|order_detail| order_detail.making_status=="in_production"}
+      order.update(order_status: 2)
+    elsif @order/prder_details.all?{|order_detail| order_detail.maiking_status=="production_completes"}
+      @order.update(order_status: 3)
+    end
+    redirect_to admin_order_path(params[:order_id])
+      flash[:notice] = "製作ステータス更新しました"
+
+  end
+
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
 end
