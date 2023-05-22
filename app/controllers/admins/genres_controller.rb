9c3134005fa2aead0c6635e62bd5ac3f319811bc
EC2 Default User <ec2-user@ip-172-31-17-4.ap-northeast-1.compute.internal>
2023-05-22 09:13:04 +0000

    [update,add]バリテーション機能実装,edit作成

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -10,7 +10,21 @@ class Admins::GenresController < ApplicationController
     if @genre.save
       redirect_to admins_genres_path(@genre), notice: "商品を登録しました。"
     else
+      @genres = Genre.all
       render :index
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admins_genres_path, notice: "ジャンルを編集しました。"
+    else
+      render 'edit'
     end
   end
 
