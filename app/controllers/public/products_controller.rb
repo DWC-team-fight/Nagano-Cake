80dcaa56479ea270506e3012aba6804eb6d36456
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-05-24 05:33:08 +0000

    update

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,5 +1,5 @@
 class Public::ProductsController < ApplicationController
-  
+
   def index
     @total_products = Product.all
     @products = Product.all.page(params[:page]).per(8)
@@ -7,18 +7,12 @@ class Public::ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    
+
     @cart_item = CartItem.new
   end
 
   private
   def products_params
-<<<<<<< HEAD
     params.require(:product).permit(:genle_id, :name, :introduction, :price, :is_status, :image_id, :image)
   end
-=======
-    params.require(:product).permit(:genre_id, :name, :introduction, :price, :is_status, :image_id, :image)
-  end
-  
->>>>>>> origin/develop
 end
