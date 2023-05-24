136c3c8744c7a4ccfb2bcfcc47d24be3d2b265eb
PB-193 <bigbamboopbo@yahoo.co.jp>
2023-05-24 07:36:22 +0000

    update

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,14 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :products, through: :order_details
 
-  enum payment_method: { credit_card: 0, transfer: 1 }
+  # バリデーション
+  validates :delivery_postal_code, format: {with: /\A[0-9]{7}\z/}
+  validates :delivery_address,presence:true
+  validates :delivery_name,presence:true
+  validates :payment_method,presence:true
 
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: {
     payment_pending: 0,
     payment_confirmation: 1,
