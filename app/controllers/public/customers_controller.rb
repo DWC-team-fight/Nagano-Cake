795f9ea6d7d7fc45238e60b5b5d267b43f6b3490
chihiro0401 <129600067+chihiro0401@users.noreply.github.com>
2023-05-24 09:32:31 +0000

    編集画面完了

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,11 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = current_customer
   end
-  
-  #登録情報へのアクション 
+
+  #登録情報へのアクション
   def edit
     @customer = current_customer
   end
@@ -15,16 +15,19 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
     if @customer.update(customer_params)
         flash[:notice] = "登録情報が更新されました。"
-      redirect_to publics_customers_mypage
+      redirect_to public_customers_mypage_path
     else
-      render 'edit'
+      render :edit
     end
   end
 
-  # 退会アクション
-  def withdraw
+
+  def check
+  end
+
+  def withdrawal
      @customer = current_customer
-     @customer.update(is_customer_status: true)
+     @customer.update(is_deleted: true)
      reset_session
      flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
      redirect_to root_path
