01d8a77772b16cc29669e2f82d767d1ea5500e8a
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-05-26 05:26:39 +0000

    update

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,10 +3,10 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-    enum product_order_status: {
+  enum product_order_status: {
     impossible_work: 0,
     waiting_work: 1,
     working: 2,
     finish: 3,
-    }
+  }
 end
